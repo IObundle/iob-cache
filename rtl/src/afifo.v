7e983fa38c1272f8344b32823e23899db70e58b0
João Roque <joao_v_roque@hotmail.com>
2019-11-22 18:23:08 +0000

    Updated with the new Cache developed in iob-cache Master repo

--- rtl/src/afifo.v
+++ rtl/src/afifo.v
@@ -1,146 +1,139 @@
-//==========================================
-// Function : Asynchronous FIFO (w/ 2 asynchronous clocks).
-// Coder    : Alex Claros F.
-// Date     : 15/May/2005.
-// Notes    : This implementation is based on the article 
-//            'Asynchronous FIFO in Virtex-II FPGAs'
-//            writen by Peter Alfke. This TechXclusive 
-//            article can be downloaded from the
-//            Xilinx website. It has some minor modifications.
-//=========================================
-
 `timescale 1ns/1ps
 
-module aFifo
-  #(parameter    DATA_WIDTH    = 8,
-    ADDRESS_WIDTH = 4,
-    FIFO_DEPTH    = (1 << ADDRESS_WIDTH))
-   //Reading port
-   (output reg  [DATA_WIDTH-1:0]        Data_out, 
-    output reg                  Empty_out,
-    input wire                  ReadEn_in,
-    input wire                  RClk, 
-    //Writing port.	 
-    input wire [DATA_WIDTH-1:0] Data_in, 
-    output reg                  Full_out,
-    input wire                  WriteEn_in,
-    input wire                  WClk,
-   
-    input wire                  Clear_in);
+module gray_counter #(parameter   COUNTER_WIDTH = 4)
+   (
+    input wire 		       rst, //Count reset.
+    input wire 		       clk,
+    input wire 		       en, //Count enable.
+    output [COUNTER_WIDTH-1:0] data_out
+    );
 
-   /////Internal connections & variables//////
-  reg   [DATA_WIDTH-1:0]              Mem [FIFO_DEPTH-1:0];
-   wire [ADDRESS_WIDTH-1:0]           pNextWordToWrite, pNextWordToRead;
-   wire                               EqualAddresses;
-   wire                               NextWriteAddressEn, NextReadAddressEn;
-   wire                               Set_Status, Rst_Status;
-   reg                                Status;
-   wire                               PresetFull, PresetEmpty;
-   
-   //////////////Code///////////////
-     //Data ports logic:
-   //(Uses a dual-port RAM).
-   //'Data_out' logic:
-   always @ (posedge RClk)
-     if (ReadEn_in & !Empty_out)
-       Data_out <= Mem[pNextWordToRead];
-   
-   //'Data_in' logic:
-   always @ (posedge WClk)
-     if (WriteEn_in & !Full_out)
-       Mem[pNextWordToWrite] <= Data_in;
+   reg [COUNTER_WIDTH-1:0]     bin_counter;
+   reg [COUNTER_WIDTH-1:0]     gray_counter;
 
-   //Fifo addresses support logic: 
-   //'Next Addresses' enable logic:
-   assign NextWriteAddressEn = WriteEn_in & ~Full_out;
-   assign NextReadAddressEn  = ReadEn_in  & ~Empty_out;
+   assign data_out = gray_counter;
    
-   //Addreses (Gray counters) logic:
-   GrayCounter #(ADDRESS_WIDTH) GrayCounter_pWr (
-                                                 .GrayCount_out(pNextWordToWrite),
-                                                 .Enable_in(NextWriteAddressEn),
-                                                 .Clear_in(Clear_in),
-                                                 .Clk(WClk)
-                                                 );
-   
-   GrayCounter #(ADDRESS_WIDTH) GrayCounter_pRd (
-                                                 .GrayCount_out(pNextWordToRead),
-                                                 .Enable_in(NextReadAddressEn),
-                                                 .Clear_in(Clear_in),
-                                                 .Clk(RClk)
-                                                 );
-   
-
-   //'EqualAddresses' logic:
-   assign EqualAddresses = (pNextWordToWrite == pNextWordToRead);
-
-   //'Quadrant selectors' logic:
-   assign Set_Status = (pNextWordToWrite[ADDRESS_WIDTH-2] ~^ pNextWordToRead[ADDRESS_WIDTH-1]) &
-                       (pNextWordToWrite[ADDRESS_WIDTH-1] ^  pNextWordToRead[ADDRESS_WIDTH-2]);
-   
-   assign Rst_Status = (pNextWordToWrite[ADDRESS_WIDTH-2] ^  pNextWordToRead[ADDRESS_WIDTH-1]) &
-                       (pNextWordToWrite[ADDRESS_WIDTH-1] ~^ pNextWordToRead[ADDRESS_WIDTH-2]);
-   
-   //'Status' latch logic:
-   always @ (Set_Status, Rst_Status, Clear_in) //D Latch w/ Asynchronous Clear & Preset.
-     if (Rst_Status | Clear_in)
-       Status = 0;  //Going 'Empty'.
-     else if (Set_Status)
-       Status = 1;  //Going 'Full'.
-   
-   //'Full_out' logic for the writing port:
-   assign PresetFull = Status & EqualAddresses;  //'Full' Fifo.
-   
-   always @ (posedge WClk, posedge PresetFull) //D Flip-Flop w/ Asynchronous Preset.
-     if (PresetFull)
-       Full_out <= 1;
-     else
-       Full_out <= 0;
-   
-   //'Empty_out' logic for the reading port:
-   assign PresetEmpty = ~Status & EqualAddresses;  //'Empty' Fifo.
-   
-   always @ (posedge RClk, posedge PresetEmpty)  //D Flip-Flop w/ Asynchronous Preset.
-     if (PresetEmpty)
-       Empty_out <= 1;
-     else
-       Empty_out <= 0;
-   
-endmodule
-
-
-//==========================================
-// Function : Code Gray counter.
-// Coder    : Alex Claros F.
-// Date     : 15/May/2005.
-//=======================================
-
-`timescale 1ns/1ps
-
-module GrayCounter
-  #(parameter   COUNTER_WIDTH = 4)
-   
-   (output reg  [COUNTER_WIDTH-1:0]    GrayCount_out,  //'Gray' code count output.
-    
-    input wire Enable_in, //Count enable.
-    input wire Clear_in, //Count reset.
-   
-    input wire Clk);
-
-   /////////Internal connections & variables///////
-  reg    [COUNTER_WIDTH-1:0]         BinaryCount;
-
-   /////////Code///////////////////////
-    
-   always @ (posedge Clk)
-     if (Clear_in) begin
-        BinaryCount   <= {COUNTER_WIDTH{1'b 0}} + 1'b1;  //Gray count begins @ '1' with
-        GrayCount_out <= {COUNTER_WIDTH{1'b 0}};      // first 'Enable_in'.
-     end
-     else if (Enable_in) begin
-        BinaryCount   <= BinaryCount + 1'b1;
-        GrayCount_out <= {BinaryCount[COUNTER_WIDTH-1],
-                          BinaryCount[COUNTER_WIDTH-2:0] ^ BinaryCount[COUNTER_WIDTH-1:1]};
+   always @ (posedge clk, posedge rst)
+     if (rst) begin
+        bin_counter   <= 1; 
+        gray_counter <= 0; 
+     end else if (en) begin
+        bin_counter   <= bin_counter + 1'b1;
+        gray_counter <= {bin_counter[COUNTER_WIDTH-1], bin_counter[COUNTER_WIDTH-2:0] ^ bin_counter[COUNTER_WIDTH-1:1]};
      end
    
 endmodule
+
+module iob_async_fifo
+  #(parameter 
+    DATA_WIDTH = 8, 
+    ADDRESS_WIDTH = 4, 
+    FIFO_DEPTH = (1 << ADDRESS_WIDTH)
+    )
+   (
+    input 			rst,
+
+    //read port
+    output reg [DATA_WIDTH-1:0] data_out, 
+    output 			empty,
+    output [ADDRESS_WIDTH-1:0] 	level_r,
+    input 			read_en,
+    input 			rclk, 
+
+    //write port	 
+    input [DATA_WIDTH-1:0] 	data_in, 
+    output 			full,
+    output [ADDRESS_WIDTH-1:0] 	level_w,
+    input 			write_en,
+    input 			wclk
+    );
+
+   //FIFO memory
+   reg [DATA_WIDTH-1:0] 	mem [FIFO_DEPTH-1:0];
+
+   //WRITE DOMAIN 
+   wire [ADDRESS_WIDTH-1:0] 	wptr;
+   reg [ADDRESS_WIDTH-1:0] 	rptr_sync[1:0];
+   wire                         write_en_int;
+   
+   //READ DOMAIN    
+   wire [ADDRESS_WIDTH-1:0] 	rptr;
+   reg [ADDRESS_WIDTH-1:0] 	wptr_sync[1:0];
+   wire                         read_en_int;
+
+   //convert gray to binary code
+   function [ADDRESS_WIDTH-1:0] gray2bin;
+      input reg [ADDRESS_WIDTH-1:0] gr;
+      input integer                 N;
+      begin: g2b
+	 reg [ADDRESS_WIDTH-1:0] bi;
+	 integer                 i;
+	 
+	 bi[N-1] = gr[N-1];
+	 for (i=N-2;i>=0;i=i-1)
+           bi[i] = gr[i] ^ bi[i+1];
+	 
+	 gray2bin = bi;
+      end
+   endfunction
+   
+   //WRITE DOMAIN LOGIC
+
+   //sync read pointer
+   always @ (posedge wclk) begin 
+      rptr_sync[0] <= rptr;
+      rptr_sync[1] <= rptr_sync[0];
+   end
+   
+   //effective write enable
+   assign write_en_int = write_en & ~full;
+   
+   //write
+   always @ (posedge wclk)
+     if (write_en_int)
+       mem[wptr] <= data_in;
+
+   gray_counter #(ADDRESS_WIDTH) wptr_counter (
+                                               .clk(wclk),
+                                               .rst(rst), 
+                                               .en(write_en_int),
+                                               .data_out(wptr)
+                                               );
+   //compute binary pointer difference
+   assign level_w = gray2bin(wptr, ADDRESS_WIDTH) - gray2bin(rptr_sync[1], ADDRESS_WIDTH);
+   
+   assign full = (level_w == (FIFO_DEPTH-1));
+
+   
+   //READ DOMAIN LOGIC
+
+   //sync write pointer
+   always @ (posedge rclk) begin 
+      wptr_sync[0] <= wptr;
+      wptr_sync[1] <= wptr_sync[0];
+   end
+
+   //effective read enable
+   assign read_en_int  = read_en & ~empty;
+   
+   //read
+   always @ (posedge rclk)
+     if (read_en_int)
+       data_out <= mem[rptr];
+
+   gray_counter #(ADDRESS_WIDTH) rptr_counter (
+                                               .clk(rclk),
+                                               .rst(rst), 
+                                               .en(read_en_int),
+                                               .data_out(rptr)
+                                               );
+   
+   //compute binary pointer difference
+   assign level_r = gray2bin(wptr_sync[1], ADDRESS_WIDTH) - gray2bin(rptr, ADDRESS_WIDTH);
+   
+
+   assign empty = (level_r == 0);
+   
+
+endmodule
+
