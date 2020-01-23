e98b094a87e5653e70c6258f2b0a804a01dd2e06
JVRoque <joao_v_roque@hotmail.com>
2020-01-23 23:05:23 +0000

    Small fix, still needs to see if there is a way to apply both cases in the same module (generatable_memory.v)

--- rtl/src/generable_memory.v
+++ rtl/src/generable_memory.v
@@ -2,7 +2,44 @@
 
 
 module generable_memory #(
-                          parameter RESET  = 1,          // Enables Reset; If Not Used = 0
+		          parameter ADDR_W = 10,
+                          parameter DATA_W = 32,         // Total size of the memory (N_MEM x MEM_W)
+                          parameter MEM_W  = 8,    // Size of each individual memory
+                          parameter N_MEM  = DATA_W/MEM_W// Number of individual memories 
+		          )
+   (      
+	  input                clk,
+	  input [DATA_W-1:0]   mem_write_data,
+	  input [ADDR_W-1:0]   mem_addr,
+	  input [N_MEM-1:0]    mem_en,
+	  output [DATA_W-1 :0] mem_read_data
+	  );
+   
+   genvar                      i;
+   integer                     j;
+   
+   generate
+      for (i = 0; i < N_MEM; i=i+1)
+        begin
+
+           reg [MEM_W-1:0]             mem [2**ADDR_W-1:0]; //each individual memory 
+           
+           always @ (posedge clk)
+             begin // Port A
+                  if (mem_en[i])
+                    mem [mem_addr] <= mem_write_data [MEM_W*(i+1) -1: MEM_W*i];
+             end 
+           
+           assign mem_read_data [MEM_W*(i+1) -1: MEM_W*i] = mem [mem_addr];
+           
+        end                                             
+   endgenerate
+   
+endmodule
+
+
+
+module generesetable_memory #(
 		          parameter ADDR_W = 10,
                           parameter DATA_W = 32,         // Total size of the memory (N_MEM x MEM_W)
                           parameter MEM_W  = 8,    // Size of each individual memory
@@ -28,7 +65,7 @@ module generable_memory #(
            
            always @ (posedge clk)
              begin // Port A
-                if (rst & RESET)
+                if (rst)
                   for (j=0; j < 2**ADDR_W; j=j+1) //resets the entire memory
                     mem[j] <= {MEM_W{1'b0}};
                 else
