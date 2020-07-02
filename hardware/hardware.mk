de2a4da2e336fd60fecd1473250b3537c4ee6a7f
JVRoque <joao_v_roque@hotmail.com>
2020-07-02 11:19:47 +0100

    Changes to .mk for simulation to fully work

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -15,4 +15,7 @@ CACHE_SRC_DIR:=$(CACHE_DIR)/hardware/src
 VSRC+=$(wildcard $(CACHE_HW_DIR)/src/*.v) \
 $(MEM_DIR)/reg_file/iob_reg_file.v \
 $(MEM_DIR)/fifo/afifo/afifo.v \
-$(MEM_DIR)/sp_ram/iob_sp_mem.v
+$(MEM_DIR)/sp_ram_be/iob_sp_ram_be.v \
+$(MEM_DIR)/sp_ram/iob_sp_mem.v \
+$(AXI_MEM_DIR)/rtl/axi_ram.v \
+$(CACHE_HW_DIR)/wrapper/L2_ID_1sp.v
