391ff2ab7835efdafeffa928b00d0661551cdeef
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-07-02 02:30:19 +0100

    remove testbench source from exported sources

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -13,7 +13,6 @@ VHDR+=$(wildcard $(CACHE_INC_DIR)/*.vh)
 #sources
 CACHE_SRC_DIR:=$(CACHE_DIR)/hardware/src
 VSRC+=$(wildcard $(CACHE_HW_DIR)/src/*.v) \
-$(AXI_MEM_DIR)/rtl/axi_ram.v \
 $(MEM_DIR)/reg_file/iob_reg_file.v \
 $(MEM_DIR)/fifo/afifo/afifo.v \
 $(MEM_DIR)/sp_ram/iob_sp_mem.v
