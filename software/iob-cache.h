52b83d4b40cc3ad4ed42fbeeb7ce5c6a81c6df8b
P_Miranda <pedro_miranda2@hotmail.com>
2020-09-27 17:43:53 +0100

    Fixed cache control instructions
    Fixed cache_init(), requires base address to access external memory in the first place, then we can address 1<< CACHE_ADDR_W
    
    CTRL instruction memory map in sw is the same as the mapping in HW

--- software/iob-cache.h
+++ software/iob-cache.h
@@ -6,21 +6,21 @@ static int cache_base;
 #define CACHEFUNC(cache_base, func) (*((volatile int*) (cache_base + (func * sizeof(int)))))
 
 //Function's memory map
-#define INVALIDATE      0
-#define BUFFER_EMPTY    1
-#define BUFFER_FULL     2
-#define HIT             3
-#define MISS            4
-#define READ_HIT        5
-#define READ_MISS       6
-#define WRITE_HIT       7
-#define WRITE_MISS      8
-#define COUNTER_RESET   9
-#define INSTR_HIT       10 //for CTRL_CNT_ID only
-#define INSTR_MISS      11 //for CTRL_CNT_ID only
+#define INVALIDATE      1
+#define BUFFER_EMPTY    2
+#define BUFFER_FULL     3
+#define HIT             4
+#define MISS            5
+#define READ_HIT        6
+#define READ_MISS       7
+#define WRITE_HIT       8
+#define WRITE_MISS      9
+#define COUNTER_RESET   10
+#define INSTR_HIT       11 //for CTRL_CNT_ID only
+#define INSTR_MISS      12 //for CTRL_CNT_ID only
 
 // Cache Controllers's functions
-void cache_init(int cache_addr); // initialized the cache_base static integer
+void cache_init(int ext_mem, int cache_addr); // initialized the cache_base static integer
 int cache_invalidate(); 
 int cache_buffer_empty();    
 int cache_buffer_full();
