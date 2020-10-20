62beebb6b3b1c1c3965a88d622f1400216c25f4e
JVRoque <joao_v_roque@hotmail.com>
2020-10-20 18:53:06 +0100

    merge with upstream

--- software/iob-cache.h
+++ software/iob-cache.h
@@ -6,16 +6,16 @@ static int cache_base;
 #define CACHEFUNC(cache_base, func) (*((volatile int*) (cache_base + (func * sizeof(int)))))
 
 //Function's memory map
-#define INVALIDATE      1
-#define BUFFER_EMPTY    2
-#define BUFFER_FULL     3
-#define HIT             4
-#define MISS            5
-#define READ_HIT        6
-#define READ_MISS       7
-#define WRITE_HIT       8
-#define WRITE_MISS      9
-#define COUNTER_RESET   10
+#define BUFFER_EMPTY    1
+#define BUFFER_FULL     2
+#define HIT             3
+#define MISS            4
+#define READ_HIT        5
+#define READ_MISS       6
+#define WRITE_HIT       7
+#define WRITE_MISS      8
+#define COUNTER_RESET   9
+#define INVALIDATE      10
 #define INSTR_HIT       11 //for CTRL_CNT_ID only
 #define INSTR_MISS      12 //for CTRL_CNT_ID only
 
