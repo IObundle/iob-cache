685d733fd2ea70ea946fefbcb8d8a47fe3492d07
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-10-25 03:18:01 +0000

    improve simulation.mk

--- hardware/simulation/simulation.mk
+++ hardware/simulation/simulation.mk
@@ -1,15 +1,14 @@
-#core
 include $(CACHE_DIR)/hardware/hardware.mk
 
-#testbench
+#includes
+INCLUDE+=$(incdir)$(CACHE_HW_DIR)/testbench/
+
+#headers
+VHDR+=$(CACHE_HW_DIR)/testbench/iob-cache_tb.vh
+
+#sources
 VSRC+=$(CACHE_HW_DIR)/testbench/pipeline-iob-cache_tb.v \
 $(CACHE_DIR)/submodules/axi-mem/rtl/axi_ram.v \
 $(CACHE_DIR)/submodules/iob-mem/sp_ram_be/iob_sp_ram_be.v \
-#$(CACHE_HW_DIR)/wrapper/L2_ID_1sp.v \
 
-#additional includes
-INCLUDE+=$(incdir)$(CACHE_HW_DIR)/testbench/
-VHDR+=$(CACHE_HW_DIR)/testbench/iob-cache_tb.vh
 
-#additional defmacros
-DEFINE+=$(defmacro)DATA_W=32 $(defmacro)ADDR_W=32
