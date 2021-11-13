9e79da108949126d708bcb49f7c441d4f9116067
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2021-11-13 16:53:56 +0000

    disambiguating paths

--- config.mk
+++ config.mk
@@ -2,17 +2,19 @@ MODULE:=CACHE
 TOP_MODULE = iob_cache
 
 #PATHS
-REMOTE_ROOT_DIR ?= sandbox/iob-soc/submodules/CACHE
+#paths that need disambiguation by prefix CACHE_
 CACHE_HW_DIR:=$(CACHE_DIR)/hardware
 CACHE_SW_DIR:=$(CACHE_DIR)/software
+CACHE_SUBMODULES_DIR:=$(CACHE_DIR)/submodules
+#paths that need no disambiguation
+REMOTE_ROOT_DIR ?= sandbox/iob-soc/submodules/CACHE
 SIM_DIR ?=$(CACHE_HW_DIR)/simulation
 FPGA_DIR ?=$(shell find $($(MODULE)_DIR)/hardware -name $(FPGA_FAMILY))
 DOC_DIR:=$(CACHE_DIR)/document
-SUBMODULES_DIR:=$(CACHE_DIR)/submodules
 
 # SUBMODULE PATHS
-SUBMODULES_LIST:=$(shell ls $(SUBMODULES_DIR))
-$(foreach p, $(SUBMODULES_LIST), $(if $(filter $p, $(SUBMODULES)),,$(eval $p_DIR ?=$(SUBMODULES_DIR)/$p)))
+SUBMODULES_LIST:=$(shell ls $(CACHE_SUBMODULES_DIR))
+$(foreach p, $(SUBMODULES_LIST), $(if $(filter $p, $(SUBMODULES)),,$(eval $p_DIR ?=$(CACHE_SUBMODULES_DIR)/$p)))
 
 #DEFAULT FPGA FAMILY
 FPGA_FAMILY ?=CYCLONEV-GT
