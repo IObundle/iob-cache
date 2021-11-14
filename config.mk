b208b39564b58d935fe77b6045109bbe7201c6e3
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2021-11-14 17:04:30 +0000

    update config

--- config.mk
+++ config.mk
@@ -1,20 +1,20 @@
-MODULE:=CACHE
+MODULE=CACHE
 TOP_MODULE = iob_cache
 
 #PATHS
 #paths that need disambiguation by prefix CACHE_
 CACHE_HW_DIR:=$(CACHE_DIR)/hardware
 CACHE_SW_DIR:=$(CACHE_DIR)/software
-CACHE_SUBMODULES_DIR:=$(CACHE_DIR)/submodules
 #paths that need no disambiguation
+SUBMODULES_DIR=$(CACHE_DIR)/submodules
 REMOTE_ROOT_DIR ?= sandbox/iob-soc/submodules/CACHE
 SIM_DIR ?=$(CACHE_HW_DIR)/simulation
 FPGA_DIR ?=$(shell find $($(MODULE)_DIR)/hardware -name $(FPGA_FAMILY))
 DOC_DIR:=$(CACHE_DIR)/document
 
 # SUBMODULE PATHS
-SUBMODULES_LIST:=$(shell ls $(CACHE_SUBMODULES_DIR))
-$(foreach p, $(SUBMODULES_LIST), $(if $(filter $p, $(SUBMODULES)),,$(eval $p_DIR ?=$(CACHE_SUBMODULES_DIR)/$p)))
+SUBMODULES_LIST=$(shell ls $(SUBMODULES_DIR))
+$(foreach p, $(SUBMODULES_LIST), $(if $(filter $p, $(SUBMODULES)),,$(eval $p_DIR ?=$(SUBMODULES_DIR)/$p)))
 
 #DEFAULT FPGA FAMILY
 FPGA_FAMILY ?=CYCLONEV-GT
