c0951745b9ebd20ed4890ed0cf0682fa1d517290
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-01-24 13:23:38 +0000

    update LIB and MEM submodules; update sources according new LIB version

--- software/software.mk
+++ software/software.mk
@@ -1,11 +1,11 @@
 include $(CACHE_DIR)/config.mk
 
+ifneq (CACHE,$(filter CACHE, $(MODULES)))
+
 MODULES+=CACHE
 
 #SUBMODULES
-ifneq (LIB,$(filter LIB, $(MODULES)))
 include $(LIB_DIR)/software/software.mk
-endif
 
 #INCLUDE
 INCLUDE+=-I$(CACHE_DIR)/software
@@ -16,3 +16,4 @@ HDR+=$(CACHE_SW_DIR)/iob-cache.h
 #SOURCES
 SRC+=$(CACHE_SW_DIR)/iob-cache.c
 
+endif
