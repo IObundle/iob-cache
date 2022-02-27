12b0c0978bb820cc21169dc1315594893da908b3
codingUniv <salisaf185@gmail.com>
2022-02-27 03:24:45 +0000

    update root Makefile; update LIB, MEM and AXI submodules; update documentation makefiles and tex files

--- document/document.mk
+++ document/document.mk
@@ -1,23 +1,32 @@
 include $(CACHE_DIR)/config.mk
 
 #block diagram verilog source
-BD_VSRC=iob-cache.v
-CORENAME=CACHE
+BD_VSRC=iob_cache.v
 
 SWREGS=0
 
-INTEL ?=1
 INT_FAMILY ?=CYCLONEV-GT
-XILINX ?=1
 XIL_FAMILY ?=XCKU
+
+ASIC_NODE=0
 
 NOCLEAN+=-o -name "test.expected" -o -name "Makefile"
 
 #include tex submodule makefile segment
+#root directory
 CORE_DIR:=$(CACHE_DIR)
+#headers for creating tables
+VHDR+=$(FPGA_DIR)/iob_cache_swreg_def.vh
+VHDR+=$(CACHE_HW_DIR)/include/iob_cache_swreg.vh
+VHDR+=$(LIB_DIR)/hardware/include/iob_s_if.vh
+VHDR+=$(LIB_DIR)/hardware/include/gen_if.vh
+
+#export definitions
+export DEFINE
+
 include $(LIB_DIR)/document/document.mk
 
-test: clean all
+test: clean $(DOC).pdf
 	diff -q $(DOC).aux test.expected
 
 .PHONY: test
