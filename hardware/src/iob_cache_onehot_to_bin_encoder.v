1602a98f134a2f6904b060ce3fac2a7cef2e7b12
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-05-16 21:28:32 +0100

    add iob_cache_ prefix to all source files; switch '-' for '_'; format sources; add test.expected files for documentation; update documentation PDFs

--- hardware/src/iob_cache_onehot_to_bin_encoder.v
+++ hardware/src/iob_cache_onehot_to_bin_encoder.v
@@ -5,20 +5,23 @@
 /*---------------------------*/
 
 // One-hot to binary encoder (if input is (0)0 or (0)1, the output is 0)
-module onehot_to_bin 
+module iob_cache_onehot_to_bin
   #(
     parameter BIN_W = 2
     )
    (
     input [2**BIN_W-1:1]   onehot,
-    output reg [BIN_W-1:0] bin 
+    output reg [BIN_W-1:0] bin
     );
+
+   reg [BIN_W-1:0]         bin_cnt;
+   integer                 i;
+
    always @ (onehot) begin: onehot_to_binary_encoder
-      integer i;
-      reg [BIN_W-1:0] bin_cnt ;
       bin_cnt = 0;
-      for (i=1; i<2**BIN_W; i=i+1)
-        if (onehot[i]) bin_cnt = bin_cnt|i;
-      bin = bin_cnt;    
+      for (i=1; i < 2**BIN_W; i=i+1)
+        if (onehot[i]) bin_cnt = bin_cnt | i;
+      bin = bin_cnt;
    end
-endmodule  // onehot_to_bin
+
+endmodule
