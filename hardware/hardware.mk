eecec8147cb5d04946c911ea6c57b5331bcb967f
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-08-04 01:25:52 +0100

    deal with simulation.mk

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -52,25 +52,3 @@ VSRC+=$(VSRC2)
 
 $(BUILD_VSRC_DIR)/%.v: $(CACHE_DIR)/hardware/src/%.v
 	cp $< $@
-
-
-#
-# SIMULATION FILES
-#
-
-# copy simulation wrapper
-VSRC+=$(BUILD_VSRC_DIR)/iob_cache_wrapper.v
-$(BUILD_VSRC_DIR)/iob_cache_wrapper.v: $(CORE_SIM_DIR)/iob_cache_wrapper.v
-	cp $< $(BUILD_VSRC_DIR)
-
-# copy external memory for iob interface
-include hardware/ram/iob_ram_sp_be/hardware.mk
-
-# copy external memory for axi interface
-include hardware/axiram/hardware.mk
-
-# generate and copy AXI4 wires to connect cache to axi memory
-VHDR+=$(BUILD_VSRC_DIR)/iob_cache_axi_wire.vh
-$(BUILD_VSRC_DIR)/iob_cache_axi_wire.vh:
-	./software/python/axi_gen.py axi_wire iob_cache_
-	mv $(subst $(BUILD_VSRC_DIR)/, , $@) $(BUILD_VSRC_DIR)
--- hardware/simulation/simulation.mk
+++ hardware/simulation/simulation.mk
@@ -12,6 +12,23 @@ endif
 #verilator top module
 VTOP:=iob_cache_wrapper
 
+# copy simulation wrapper
+VSRC+=$(BUILD_VSRC_DIR)/iob_cache_wrapper.v
+$(BUILD_VSRC_DIR)/iob_cache_wrapper.v: $(CORE_SIM_DIR)/iob_cache_wrapper.v
+	cp $< $(BUILD_VSRC_DIR)
+
+# copy external memory for iob interface
+include hardware/ram/iob_ram_sp_be/hardware.mk
+
+# copy external memory for axi interface
+include hardware/axiram/hardware.mk
+
+# generate and copy AXI4 wires to connect cache to axi memory
+VHDR+=$(BUILD_VSRC_DIR)/iob_cache_axi_wire.vh
+$(BUILD_VSRC_DIR)/iob_cache_axi_wire.vh:
+	./software/python/axi_gen.py axi_wire iob_cache_
+	mv $(subst $(BUILD_VSRC_DIR)/, , $@) $(BUILD_VSRC_DIR)
+
 #tests
 TEST_LIST+=test1
 test1:
