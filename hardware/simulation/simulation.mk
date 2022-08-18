65e9690f80923dfe85afc4001429e67f905cf5db
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-08-18 01:37:10 +0100

    fix simulation issues

--- hardware/simulation/simulation.mk
+++ hardware/simulation/simulation.mk
@@ -2,11 +2,8 @@
 # This file is included in BUILD_DIR/sim/Makefile
 #
 
-# include core basic info
-include ../../info.mk
-
 ifeq ($(TOP_MODULE),iob_cache_axi)
-VFLAGS+=-DAXI
+DEFINE=-DAXI
 endif
 
 #verilator top module
