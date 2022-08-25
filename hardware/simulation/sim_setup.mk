17f1402a9d1be836e0aec62ef5a18a029ad7b9b9
PMiranda <pmiranda.iobundle@gmail.com>
2022-08-25 12:25:08 +0100

    fix(sim): update LIB, sim sources to hw/sim - update LIB submodule - copy simulation specific sources to `BUILD_DIR/hw/sim` during setup - add simulation specific sources in `simulation.mk` to `VSRC` and to   `NOCLEAN` variable to exclude from simulation `clean` target - this ensures that `BUILD_DIR/hw/vsrc` has no simulation specific   sources - fix CI workflow

--- hardware/simulation/sim_setup.mk
+++ hardware/simulation/sim_setup.mk
@@ -3,9 +3,9 @@
 #
 
 # copy simulation wrapper
-VSRC+=$(BUILD_VSRC_DIR)/iob_cache_wrapper.v
-$(BUILD_VSRC_DIR)/iob_cache_wrapper.v: $(CORE_SIM_DIR)/iob_cache_wrapper.v
-	cp $< $(BUILD_VSRC_DIR)
+VSRC+=$(BUILD_SIM_DIR)/iob_cache_wrapper.v
+$(BUILD_SIM_DIR)/iob_cache_wrapper.v: $(CORE_SIM_DIR)/iob_cache_wrapper.v
+	cp $< $(BUILD_SIM_DIR)
 
 # copy external memory for iob interface
 include hardware/ram/iob_ram_sp_be/hardware.mk
@@ -14,7 +14,7 @@ include hardware/ram/iob_ram_sp_be/hardware.mk
 include hardware/axiram/hardware.mk
 
 # generate and copy AXI4 wires to connect cache to axi memory
-VHDR+=$(BUILD_VSRC_DIR)/iob_cache_axi_wire.vh
-$(BUILD_VSRC_DIR)/iob_cache_axi_wire.vh:
+VHDR+=$(BUILD_SIM_DIR)/iob_cache_axi_wire.vh
+$(BUILD_SIM_DIR)/iob_cache_axi_wire.vh:
 	./software/python/axi_gen.py axi_wire iob_cache_
-	mv $(subst $(BUILD_VSRC_DIR)/, , $@) $(BUILD_VSRC_DIR)
+	mv $(subst $(BUILD_SIM_DIR)/, , $@) $(BUILD_SIM_DIR)
