caad6e245a0f137f615e163cb9b3993c6c5db868
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-08-30 11:14:03 +0100

    simplify info.mk, add config_setup.mk, update fpga test expected result and iob-lib

--- info.mk
+++ info.mk
@@ -1,14 +1,11 @@
 # core name
 NAME=iob_cache
+
 # core version 
 VERSION=0010
+
 # top-level module
 TOP_MODULE?=iob_cache_iob
-#TOP_MODULE?=iob_cache_axi
-# core path as seen from LIB's makefile
-CACHE_DIR=$(CORE_DIR)
 
-SETUP_SW=0
-SETUP_SIM=1
-SETUP_FPGA=1
-SETUP_DOC=1
+# top-level module for AXI4 backend interface
+#TOP_MODULE?=iob_cache_axi
