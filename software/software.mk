018aa9aac4faf21e44056856f5267d4303c61c31
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-01 06:53:30 +0100

    replace CACHE_DIR with CORE_DIR in setup makefiles to allow running from anywhere

--- software/software.mk
+++ software/software.mk
@@ -1,10 +1,10 @@
 # (c) 2022-Present IObundle, Lda, all rights reserved
 #
-# This makefile segment lists all software header and source files 
+# This makefile segment  is used at setup-time in $(LIB_DIR)/Makefile
 #
-# It is included in submodules/LIB/Makefile for populating the
-# build directory
+# It copies all software header and source files to $(BUILD_DIR)/sw
 #
+
 
 #
 # Embedded sources
@@ -20,18 +20,18 @@ SRC+=$(BUILD_SW_SRC_DIR)/iob_cache_swreg_emb.c
 $(BUILD_SW_SRC_DIR)/iob_cache_swreg_emb.c: iob_cache_swreg_emb.c
 	cp $< $@
 
-iob_cache_swreg.h iob_cache_swreg_emb.c: $(CACHE_DIR)/mkregs.conf
-	./software/python/mkregs.py $(NAME) $(CACHE_DIR) SW
+iob_cache_swreg.h iob_cache_swreg_emb.c: $(CORE_DIR)/mkregs.conf
+	./software/python/mkregs.py $(NAME) $(CORE_DIR) SW
 
 
-HDR1=$(wildcard $(CACHE_DIR)/software/*.h)
-SRC+=$(patsubst $(CACHE_DIR)/software/%,$(BUILD_SW_SRC_DIR)/%,$(HDR1))
-$(BUILD_SW_SRC_DIR)/%.h: $(CACHE_DIR)/software/%.h
+HDR1=$(wildcard $(CORE_DIR)/software/*.h)
+SRC+=$(patsubst $(CORE_DIR)/software/%,$(BUILD_SW_SRC_DIR)/%,$(HDR1))
+$(BUILD_SW_SRC_DIR)/%.h: $(CORE_DIR)/software/%.h
 	cp $< $@
 
-SRC1=$(wildcard $(CACHE_DIR)/software/*.c)
-SRC+=$(patsubst $(CACHE_DIR)/software/%,$(BUILD_SW_SRC_DIR)/%,$(SRC1))
-$(BUILD_SW_SRC_DIR)/%.c: $(CACHE_DIR)/software/%.c
+SRC1=$(wildcard $(CORE_DIR)/software/*.c)
+SRC+=$(patsubst $(CORE_DIR)/software/%,$(BUILD_SW_SRC_DIR)/%,$(SRC1))
+$(BUILD_SW_SRC_DIR)/%.c: $(CORE_DIR)/software/%.c
 	cp $< $@
 
 
@@ -39,7 +39,7 @@ $(BUILD_SW_SRC_DIR)/%.c: $(CACHE_DIR)/software/%.c
 # PC Emul Sources
 #
 SRC+=$(BUILD_SW_SRC_DIR)/iob_cache_swreg_pc_emul.c
-$(BUILD_SW_SRC_DIR)/iob_cache_swreg_pc_emul.c: $(CACHE_DIR)/software/pc-emul/iob_cache_swreg_pc_emul.c
+$(BUILD_SW_SRC_DIR)/iob_cache_swreg_pc_emul.c: $(CORE_DIR)/software/pc-emul/iob_cache_swreg_pc_emul.c
 	cp $< $@
 
 
