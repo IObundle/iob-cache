cf572610f9d304c68420b30aa55645bbce811989
PMiranda <pmiranda.iobundle@gmail.com>
2022-09-15 11:40:29 +0100

    fix(setup): update LIB scripts path - update LIB submodule - update makefiles with new LIB/scripts path

--- hardware/simulation/sim_setup.mk
+++ hardware/simulation/sim_setup.mk
@@ -11,6 +11,6 @@ include $(LIB_DIR)/hardware/axiram/hardware.mk
 # generate and copy AXI4 wires to connect cache to axi memory
 SRC+=$(BUILD_SIM_DIR)/iob_cache_axi_wire.vh
 $(BUILD_SIM_DIR)/iob_cache_axi_wire.vh:
-	$(LIB_DIR)/software/python/axi_gen.py axi_wire iob_cache_
+	$(LIB_DIR)/scripts/axi_gen.py axi_wire iob_cache_
 	mv iob_cache_axi_wire.vh $(BUILD_SIM_DIR)
 
