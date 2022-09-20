67cf1a5a5c24df534208b0b1abc527b41570b3da
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-20 19:37:12 +0100

    doc build ok

--- hardware/hw_setup.mk
+++ hardware/hw_setup.mk
@@ -22,38 +22,38 @@ AXI_GEN:= $(LIB_DIR)/scripts/axi_gen.py
 
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_axi_m_port.vh
 $(BUILD_VSRC_DIR)/iob_cache_axi_m_port.vh:
-	$(AXI_GEN) axi_m_port iob_cache_ && mv iob_cache_axi_m_port.vh $(BUILD_VSRC_DIR)
+	$(AXI_GEN) axi_m_port iob_cache_ && cp iob_cache_axi_m_port.vh $(BUILD_VSRC_DIR)
 
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_ram_axi_portmap.vh
 $(BUILD_VSRC_DIR)/iob_cache_ram_axi_portmap.vh:
-	$(AXI_GEN) axi_portmap iob_cache_ram_ s_ && mv iob_cache_ram_axi_portmap.vh $(BUILD_VSRC_DIR)
+	$(AXI_GEN) axi_portmap iob_cache_ram_ s_ && cp iob_cache_ram_axi_portmap.vh $(BUILD_VSRC_DIR)
 
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_axi_portmap.vh
 $(BUILD_VSRC_DIR)/iob_cache_axi_portmap.vh:
-	$(AXI_GEN) axi_portmap iob_cache_ && mv iob_cache_axi_portmap.vh $(BUILD_VSRC_DIR)
+	$(AXI_GEN) axi_portmap iob_cache_ && cp iob_cache_axi_portmap.vh $(BUILD_VSRC_DIR)
 
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_m_axi_m_write_port.vh
 $(BUILD_VSRC_DIR)/iob_cache_m_axi_m_write_port.vh:
-	$(AXI_GEN) axi_m_write_port iob_cache_m_ && mv iob_cache_m_axi_m_write_port.vh $(BUILD_VSRC_DIR)
+	$(AXI_GEN) axi_m_write_port iob_cache_m_ && cp iob_cache_m_axi_m_write_port.vh $(BUILD_VSRC_DIR)
 
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_m_axi_write_portmap.vh
 $(BUILD_VSRC_DIR)/iob_cache_m_axi_write_portmap.vh:
-	$(AXI_GEN) axi_write_portmap iob_cache_m_ && mv iob_cache_m_axi_write_portmap.vh $(BUILD_VSRC_DIR)
+	$(AXI_GEN) axi_write_portmap iob_cache_m_ && cp iob_cache_m_axi_write_portmap.vh $(BUILD_VSRC_DIR)
 
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_m_axi_m_read_port.vh
 $(BUILD_VSRC_DIR)/iob_cache_m_axi_m_read_port.vh:
-	$(AXI_GEN) axi_m_read_port iob_cache_m_ && mv iob_cache_m_axi_m_read_port.vh $(BUILD_VSRC_DIR)
+	$(AXI_GEN) axi_m_read_port iob_cache_m_ && cp iob_cache_m_axi_m_read_port.vh $(BUILD_VSRC_DIR)
 
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_m_axi_read_portmap.vh
 $(BUILD_VSRC_DIR)/iob_cache_m_axi_read_portmap.vh:
-	$(AXI_GEN) axi_read_portmap iob_cache_m_ && mv iob_cache_m_axi_read_portmap.vh $(BUILD_VSRC_DIR)
+	$(AXI_GEN) axi_read_portmap iob_cache_m_ && cp iob_cache_m_axi_read_portmap.vh $(BUILD_VSRC_DIR)
 
 
 #generate software accessible register
 #note that this IP does not use the generated iob_cache_swreg_gen.vh as it provides this functionality itself
 SRC+=$(BUILD_VSRC_DIR)/iob_cache_swreg_def.vh $(BUILD_VSRC_DIR)/iob_cache_swreg_gen.vh
 $(BUILD_VSRC_DIR)/iob_cache_swreg_%.vh: iob_cache_swreg_%.vh
-	mv $< $@
+	cp $< $@
 
 iob_cache_swreg_def.vh iob_cache_swreg_gen.vh: $(CACHE_DIR)/mkregs.conf
 	$(LIB_DIR)/scripts/mkregs.py iob_cache $(CACHE_DIR) HW
