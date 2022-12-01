d8b13ab886a29255d2a2233be15bc7ceffa16150
Artur Nóbrega <arturnobrega1999@gmail.com>
2022-12-01 07:56:13 +0000

    BREAKING(setup.mk): modified _setup.mk to match iob-uart - Modified hw_setup.mk and sw_setup.mk file to match ones from iob-uart.   - This may break the build process for this component   - I need to investigate more about the iob-uart build process to     ensure it also works for the cache.   - These changes allow for iob-soc/build-lib to create the build directory,     but it currently does not run in simulation. - Update LIB

--- software/sw_setup.mk
+++ software/sw_setup.mk
@@ -1,23 +1,15 @@
 # (c) 2022-Present IObundle, Lda, all rights reserved
 #
-# This makefile segment  is used at setup-time in $(LIB_DIR)/Makefile
+# This makefile segment lists all software header and source files 
 #
-# It copies all software header and source files to $(BUILD_DIR)/sw
+# It is included in submodules/LIB/Makefile for populating the
+# build directory
 #
 
-# sw accessible registers C header and source files
-# pc-emul sources
-SRC+=$(BUILD_PSRC_DIR)/iob_cache_swreg.h
+ifeq ($(filter CACHE, $(SW_MODULES)),)
 
-$(BUILD_PSRC_DIR)/iob_cache_swreg.h: iob_cache_swreg.h
-	cp $< $@
+#add itself to SW_MODULES list
+SW_MODULES+=CACHE
 
-# embedded sources
-SRC+=$(BUILD_ESRC_DIR)/iob_cache_swreg.h $(BUILD_ESRC_DIR)/iob_cache_swreg_emb.c
 
-$(BUILD_ESRC_DIR)/iob_cache_swreg%: iob_cache_swreg%
-	mv $< $@
-
-iob_cache_swreg.h iob_cache_swreg_emb.c: $(CACHE_DIR)/mkregs.toml
-	$(LIB_DIR)/scripts/mkregs.py iob_cache $(CACHE_DIR) SW
-
+endif
