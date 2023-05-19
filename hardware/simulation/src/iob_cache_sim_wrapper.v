451da6dbefaf71fb7c01e49cd53cc630f4d87d91
Pedro Antunes <pedronmantunes@gmail.com>
2023-05-19 18:06:05 +0200

    *: remove macros; pass verible linter/formatter;

--- hardware/simulation/src/iob_cache_sim_wrapper.v
+++ hardware/simulation/src/iob_cache_sim_wrapper.v
@@ -4,139 +4,126 @@
 `include "iob_cache.vh"
 `include "iob_cache_conf.vh"
 
-module iob_cache_sim_wrapper
-  #(
+module iob_cache_sim_wrapper #(
 `ifdef AXI
-    parameter AXI_ID_W = `IOB_CACHE_AXI_ID_W,
-    parameter AXI_LEN_W = `IOB_CACHE_AXI_LEN_W,
-    parameter AXI_ADDR_W = BE_ADDR_W,
-    parameter AXI_DATA_W = BE_DATA_W,
-    parameter [AXI_ID_W-1:0] AXI_ID = `IOB_CACHE_AXI_ID,
+   parameter                AXI_ID_W      = `IOB_CACHE_AXI_ID_W,
+   parameter                AXI_LEN_W     = `IOB_CACHE_AXI_LEN_W,
+   parameter                AXI_ADDR_W    = BE_ADDR_W,
+   parameter                AXI_DATA_W    = BE_DATA_W,
+   parameter [AXI_ID_W-1:0] AXI_ID        = `IOB_CACHE_AXI_ID,
 `endif
-    parameter ADDR_W = `IOB_CACHE_FE_ADDR_W,
-    parameter DATA_W = `IOB_CACHE_FE_DATA_W,
-    parameter BE_ADDR_W = `IOB_CACHE_BE_ADDR_W,
-    parameter BE_DATA_W = `IOB_CACHE_BE_DATA_W,
-    parameter NWAYS_W = `IOB_CACHE_NWAYS_W,
-    parameter NLINES_W = `IOB_CACHE_NLINES_W,
-    parameter WORD_OFFSET_W = `IOB_CACHE_WORD_OFFSET_W,
-    parameter WTBUF_DEPTH_W = `IOB_CACHE_WTBUF_DEPTH_W,
-    parameter REP_POLICY = `IOB_CACHE_REP_POLICY,
-    parameter WRITE_POL = `IOB_CACHE_WRITE_THROUGH,
-    parameter USE_CTRL = `IOB_CACHE_USE_CTRL,
-    parameter USE_CTRL_CNT = `IOB_CACHE_USE_CTRL_CNT
-    )
-   (
-    // Front-end interface (IOb native slave)
-    `IOB_INPUT(req, 1),
-    `IOB_INPUT(addr, USE_CTRL+ADDR_W-`IOB_CACHE_NBYTES_W),
-    `IOB_INPUT(wdata, DATA_W),
-    `IOB_INPUT(wstrb, `IOB_CACHE_NBYTES),
-    `IOB_OUTPUT(rdata, DATA_W),
-    `IOB_OUTPUT(ack,1),
+   parameter                ADDR_W        = `IOB_CACHE_FE_ADDR_W,
+   parameter                DATA_W        = `IOB_CACHE_FE_DATA_W,
+   parameter                BE_ADDR_W     = `IOB_CACHE_BE_ADDR_W,
+   parameter                BE_DATA_W     = `IOB_CACHE_BE_DATA_W,
+   parameter                NWAYS_W       = `IOB_CACHE_NWAYS_W,
+   parameter                NLINES_W      = `IOB_CACHE_NLINES_W,
+   parameter                WORD_OFFSET_W = `IOB_CACHE_WORD_OFFSET_W,
+   parameter                WTBUF_DEPTH_W = `IOB_CACHE_WTBUF_DEPTH_W,
+   parameter                REP_POLICY    = `IOB_CACHE_REP_POLICY,
+   parameter                WRITE_POL     = `IOB_CACHE_WRITE_THROUGH,
+   parameter                USE_CTRL      = `IOB_CACHE_USE_CTRL,
+   parameter                USE_CTRL_CNT  = `IOB_CACHE_USE_CTRL_CNT
+) (
+   // Front-end interface (IOb native slave)
+   input  [                                  1-1:0] req,
+   input  [USE_CTRL+ADDR_W-`IOB_CACHE_NBYTES_W-1:0] addr,
+   input  [                             DATA_W-1:0] wdata,
+   input  [                  `IOB_CACHE_NBYTES-1:0] wstrb,
+   output [                             DATA_W-1:0] rdata,
+   output [                                  1-1:0] ack,
 
-    // Cache invalidate and write-trough buffer IO chain
-    `IOB_INPUT(invalidate_in, 1),
-    `IOB_OUTPUT(invalidate_out, 1),
-    `IOB_INPUT(wtb_empty_in, 1),
-    `IOB_OUTPUT(wtb_empty_out, 1),
+   // Cache invalidate and write-trough buffer IO chain
+   input  [1-1:0] invalidate_in,
+   output [1-1:0] invalidate_out,
+   input  [1-1:0] wtb_empty_in,
+   output [1-1:0] wtb_empty_out,
 
    //General Interface Signals
-   `IOB_INPUT(clk_i,          1), //V2TEX_IO System clock input.
-   `IOB_INPUT(rst_i,         1)   //V2TEX_IO System reset, active high.
-   );	
+   input [1-1:0] clk_i,  //V2TEX_IO System clock input.
+   input [1-1:0] rst_i   //V2TEX_IO System reset, active high.
+);
 
 `ifdef AXI
- `include "iob_cache_axi_wire.vh"
-`else 
-   wire                                                  be_req;
-   reg                                                   be_ack;
-   wire [`IOB_CACHE_BE_ADDR_W-1:0]                       be_addr;
-   wire [`IOB_CACHE_BE_DATA_W-1:0]                       be_wdata;
-   wire [`IOB_CACHE_BE_DATA_W/8-1:0]                     be_wstrb;
-   wire [`IOB_CACHE_BE_DATA_W-1:0]                       be_rdata;
+   `include "iob_cache_axi_wire.vh"
+`else
+   wire                              be_req;
+   reg                               be_ack;
+   wire [  `IOB_CACHE_BE_ADDR_W-1:0] be_addr;
+   wire [  `IOB_CACHE_BE_DATA_W-1:0] be_wdata;
+   wire [`IOB_CACHE_BE_DATA_W/8-1:0] be_wstrb;
+   wire [  `IOB_CACHE_BE_DATA_W-1:0] be_rdata;
 `endif
 
-   iob_cache
-       #(
-         .FE_ADDR_W(ADDR_W),
-         .FE_DATA_W(DATA_W),
-         .BE_ADDR_W(`IOB_CACHE_BE_ADDR_W),
-         .BE_DATA_W(`IOB_CACHE_BE_DATA_W),
-         .NWAYS_W(`IOB_CACHE_NWAYS_W),
-         .NLINES_W(`IOB_CACHE_NLINES_W),
-         .WORD_OFFSET_W(`IOB_CACHE_WORD_OFFSET_W),
-         .WTBUF_DEPTH_W(`IOB_CACHE_WTBUF_DEPTH_W),
-         .WRITE_POL(`IOB_CACHE_WRITE_POL),
-         .REP_POLICY(`IOB_CACHE_REP_POLICY),
-         .USE_CTRL(`IOB_CACHE_USE_CTRL)
-         )
-   cache
-     (
+   iob_cache #(
+      .FE_ADDR_W    (ADDR_W),
+      .FE_DATA_W    (DATA_W),
+      .BE_ADDR_W    (`IOB_CACHE_BE_ADDR_W),
+      .BE_DATA_W    (`IOB_CACHE_BE_DATA_W),
+      .NWAYS_W      (`IOB_CACHE_NWAYS_W),
+      .NLINES_W     (`IOB_CACHE_NLINES_W),
+      .WORD_OFFSET_W(`IOB_CACHE_WORD_OFFSET_W),
+      .WTBUF_DEPTH_W(`IOB_CACHE_WTBUF_DEPTH_W),
+      .WRITE_POL    (`IOB_CACHE_WRITE_POL),
+      .REP_POLICY   (`IOB_CACHE_REP_POLICY),
+      .USE_CTRL     (`IOB_CACHE_USE_CTRL)
+   ) cache (
       //front-end
-      .wdata (wdata),
-      .addr  (addr),
-      .wstrb (wstrb),
-      .rdata (rdata),
-      .req (req),
-      .ack (ack),
-      
+      .wdata(wdata),
+      .addr (addr),
+      .wstrb(wstrb),
+      .rdata(rdata),
+      .req  (req),
+      .ack  (ack),
+
       //invalidate / wtb empty
-      .invalidate_in(1'b0),
+      .invalidate_in (1'b0),
       .invalidate_out(),
-      .wtb_empty_in(1'b1),
-      .wtb_empty_out(),
+      .wtb_empty_in  (1'b1),
+      .wtb_empty_out (),
 
       //back-end
 `ifdef AXI
- `include "iob_cache_axi_m_portmap.vh"
+      `include "iob_cache_axi_m_portmap.vh"
 `else
-      .be_addr(be_addr),
+      .be_addr (be_addr),
       .be_wdata(be_wdata),
       .be_wstrb(be_wstrb),
       .be_rdata(be_rdata),
-      .be_req(be_req),
-      .be_ack(be_ack),
+      .be_req  (be_req),
+      .be_ack  (be_ack),
 `endif
-      .clk_i (clk_i),
-      .rst_i (rst_i)
-        );
+      .clk_i   (clk_i),
+      .rst_i   (rst_i)
+   );
 
-`ifdef AXI  
-   axi_ram 
-     #(
-       .ID_WIDTH(`IOB_CACHE_AXI_ID_W),
-       .LEN_WIDTH(`IOB_CACHE_AXI_LEN_W),
-       .DATA_WIDTH (`IOB_CACHE_BE_DATA_W),
-       .ADDR_WIDTH (`IOB_CACHE_BE_ADDR_W)
-       )
-   axi_ram
-     (
- `include "iob_cache_ram_axi_portmap.vh"
-      .clk            (clk_i),
-      .rst            (rst_i)
-      );
+`ifdef AXI
+   axi_ram #(
+      .ID_WIDTH  (`IOB_CACHE_AXI_ID_W),
+      .LEN_WIDTH (`IOB_CACHE_AXI_LEN_W),
+      .DATA_WIDTH(`IOB_CACHE_BE_DATA_W),
+      .ADDR_WIDTH(`IOB_CACHE_BE_ADDR_W)
+   ) axi_ram (
+      `include "iob_cache_ram_axi_portmap.vh"
+      .clk(clk_i),
+      .rst(rst_i)
+   );
 `else
-   iob_ram_sp_be 
-     #(
-       .DATA_W(`IOB_CACHE_BE_DATA_W),
-       .ADDR_W(`IOB_CACHE_BE_ADDR_W)
-       )
-   native_ram
-     (
-      .clk_i  (clk_i),
-      .en_i   (be_req),
-      .we_i   (be_wstrb),
-      .addr_i (be_addr),
-      .d_o    (be_rdata),
-      .d_i    (be_wdata)
-      );
+   iob_ram_sp_be #(
+      .DATA_W(`IOB_CACHE_BE_DATA_W),
+      .ADDR_W(`IOB_CACHE_BE_ADDR_W)
+   ) native_ram (
+      .clk_i (clk_i),
+      .en_i  (be_req),
+      .we_i  (be_wstrb),
+      .addr_i(be_addr),
+      .d_o   (be_rdata),
+      .d_i   (be_wdata)
+   );
 
    always @(posedge clk_i, posedge rst_i)
-     if(rst_i) 
-       be_ack <= 1'b0;
-     else
-       be_ack <= be_req;
+      if (rst_i) be_ack <= 1'b0;
+      else be_ack <= be_req;
 `endif
 
 endmodule
--- hardware/src/iob_cache_read_channel_axi.v
+++ hardware/src/iob_cache_read_channel_axi.v
@@ -3,206 +3,184 @@
 `include "iob_lib.vh"
 `include "iob_cache.vh"
 
-module iob_cache_read_channel_axi
-  #(
-    parameter ADDR_W = `IOB_CACHE_ADDR_W,
-    parameter DATA_W = `IOB_CACHE_DATA_W,
-    parameter BE_ADDR_W = `IOB_CACHE_BE_ADDR_W,
-    parameter BE_DATA_W = `IOB_CACHE_BE_DATA_W,
-    parameter WORD_OFFSET_W = `IOB_CACHE_WORD_OFFSET_W,
-    parameter AXI_ID_W = `IOB_CACHE_AXI_ID_W,
-    parameter AXI_LEN_W = `IOB_CACHE_AXI_LEN_W,
-    parameter AXI_ADDR_W = BE_ADDR_W,
-    parameter AXI_DATA_W = BE_DATA_W,
-    parameter [AXI_ID_W-1:0] AXI_ID = `IOB_CACHE_AXI_ID
-    )
-   (
-    input                                    replace_valid,
-    input [ADDR_W-1:`IOB_CACHE_BE_NBYTES_W+`IOB_CACHE_LINE2BE_W] replace_addr,
-    output reg                               replace,
-    output                                   read_valid,
-    output reg [`IOB_CACHE_LINE2BE_W-1:0]              read_addr,
-    output [BE_DATA_W-1:0]                   read_rdata,
-`include "iob_axi_m_read_port.vh"
-    input                                    clk_i,
-    input                                    reset
-    );
+module iob_cache_read_channel_axi #(
+   parameter                ADDR_W        = `IOB_CACHE_ADDR_W,
+   parameter                DATA_W        = `IOB_CACHE_DATA_W,
+   parameter                BE_ADDR_W     = `IOB_CACHE_BE_ADDR_W,
+   parameter                BE_DATA_W     = `IOB_CACHE_BE_DATA_W,
+   parameter                WORD_OFFSET_W = `IOB_CACHE_WORD_OFFSET_W,
+   parameter                AXI_ID_W      = `IOB_CACHE_AXI_ID_W,
+   parameter                AXI_LEN_W     = `IOB_CACHE_AXI_LEN_W,
+   parameter                AXI_ADDR_W    = BE_ADDR_W,
+   parameter                AXI_DATA_W    = BE_DATA_W,
+   parameter [AXI_ID_W-1:0] AXI_ID        = `IOB_CACHE_AXI_ID
+) (
+   input                                                             replace_valid,
+   input      [ADDR_W-1:`IOB_CACHE_BE_NBYTES_W+`IOB_CACHE_LINE2BE_W] replace_addr,
+   output reg                                                        replace,
+   output                                                            read_valid,
+   output reg [                            `IOB_CACHE_LINE2BE_W-1:0] read_addr,
+   output     [                                       BE_DATA_W-1:0] read_rdata,
+   `include "iob_axi_m_read_port.vh"
+   input                                                             clk_i,
+   input                                                             reset
+);
 
-   reg                                       axi_arvalid_int;
-   reg                                       axi_rready_int;
+   reg axi_arvalid_int;
+   reg axi_rready_int;
 
    assign axi_arvalid_o = axi_arvalid_int;
-   assign axi_rready_o = axi_rready_int;
-   
-   
+   assign axi_rready_o  = axi_rready_int;
+
+
    generate
       if (`IOB_CACHE_LINE2BE_W > 0) begin
          // Constant AXI signals
-         assign axi_arid_o    = `IOB_CACHE_AXI_ID;
-         assign axi_arlock_o  = 1'b0;
+         assign axi_arid_o = `IOB_CACHE_AXI_ID;
+         assign axi_arlock_o = 1'b0;
          assign axi_arcache_o = 4'b0011;
-         assign axi_arprot_o  = 3'd0;
-         assign axi_arqos_o   = 4'd0;
+         assign axi_arprot_o = 3'd0;
+         assign axi_arqos_o = 4'd0;
 
          // Burst parameters
-	 assign axi_arlen_o   = 2**`IOB_CACHE_LINE2BE_W - 1'b1; // will choose the burst lenght depending on the cache's and slave's data width
-	 assign axi_arsize_o  = `IOB_CACHE_BE_NBYTES_W;         // each word will be the width of the memory for maximum bandwidth
-         assign axi_arburst_o = 2'b01;                // incremental burst
+         assign axi_arlen_o   = 2**`IOB_CACHE_LINE2BE_W - 1'b1; // will choose the burst lenght depending on the cache's and slave's data width
+         assign axi_arsize_o  = `IOB_CACHE_BE_NBYTES_W;         // each word will be the width of the memory for maximum bandwidth
+         assign axi_arburst_o = 2'b01;  // incremental burst
          assign axi_araddr_o  = {BE_ADDR_W{1'b0}} + {replace_addr, {(`IOB_CACHE_LINE2BE_W+`IOB_CACHE_BE_NBYTES_W){1'b0}}}; // base address for the burst, with width extension
 
          // Read Line values
          assign read_rdata = axi_rdata_i;
          assign read_valid = axi_rvalid_i;
 
-         localparam
-           idle          = 2'd0,
-           init_process  = 2'd1,
-           load_process  = 2'd2,
-           end_process   = 2'd3;
+         localparam idle = 2'd0, init_process = 2'd1, load_process = 2'd2, end_process = 2'd3;
 
-         reg [1:0]                           state;
+         reg [1:0] state;
          reg                                 slave_error; // axi slave_error during reply (axi_rresp[1] == 1) - burst can't be interrupted, so a flag needs to be active
 
          always @(posedge clk_i, posedge reset) begin
             if (reset) begin
-               state <= idle;
-               read_addr <= 0;
+               state       <= idle;
+               read_addr   <= 0;
                slave_error <= 0;
             end else begin
                slave_error <= slave_error;
 
                case (state)
-                 idle: begin
-                    slave_error <= 0;
-                    read_addr <= 0;
-                    if (replace_valid)
-                      state <= init_process;
-                    else
-                      state <= idle;
-                 end
-                 init_process: begin
-                    slave_error <= 0;
-                    read_addr <= 0;
-                    if (axi_arready_i)
-                      state <= load_process;
-                    else
-                      state <= init_process;
-                 end
-                 load_process: begin
-                    if (axi_rvalid_i)
-                      if (axi_rlast_i) begin
-                         state <= end_process;
-                         read_addr <= read_addr; // to avoid writting last data in first line word
-                         if (axi_rresp_i != 2'b00) // slave_error - received at the same time as the valid - needs to wait until the end to start all over - going directly to init_process would cause a stall to this burst
-                           slave_error <= 1;
-                      end else begin
-                         read_addr <= read_addr + 1'b1;
-                         state <= load_process;
-                         if (axi_rresp_i != 2'b00) // slave_error - received at the same time as the valid - needs to wait until the end to start all over - going directly to init_process would cause a stall to this burst
-                           slave_error <= 1;
-                      end else begin
-                         read_addr <= read_addr;
-                         state <= load_process;
-                      end
-                 end
-                 default: // end_process - delay for the read_latency of the memories (if the rdata is the last word)
-                   if (slave_error)
-                     state <= init_process;
-                   else
-                     state <= idle;
+                  idle: begin
+                     slave_error <= 0;
+                     read_addr   <= 0;
+                     if (replace_valid) state <= init_process;
+                     else state <= idle;
+                  end
+                  init_process: begin
+                     slave_error <= 0;
+                     read_addr   <= 0;
+                     if (axi_arready_i) state <= load_process;
+                     else state <= init_process;
+                  end
+                  load_process: begin
+                     if (axi_rvalid_i)
+                        if (axi_rlast_i) begin
+                           state     <= end_process;
+                           // to avoid writting last data in first line word
+                           read_addr <= read_addr;
+                           // slave_error - received at the same time as the valid - needs to wait until the end to start all over - going directly to init_process would cause a stall to this burst
+                           if (axi_rresp_i != 2'b00) slave_error <= 1;
+                        end else begin
+                           read_addr <= read_addr + 1'b1;
+                           state     <= load_process;
+                           // slave_error - received at the same time as the valid - needs to wait until the end to start all over - going directly to init_process would cause a stall to this burst
+                           if (axi_rresp_i != 2'b00) slave_error <= 1;
+                        end
+                     else begin
+                        read_addr <= read_addr;
+                        state     <= load_process;
+                     end
+                  end
+                  // end_process - delay for the read_latency of the memories (if the rdata is the last word)
+                  default: begin
+                     if (slave_error) state <= init_process;
+                     else state <= idle;
+                  end
                endcase
             end
          end
 
          always @* begin
             axi_arvalid_int = 1'b0;
-            axi_rready_int = 1'b0;
-            replace = 1'b1;
+            axi_rready_int  = 1'b0;
+            replace         = 1'b1;
 
             case (state)
-              idle:
-                replace = 1'b0;
-              init_process:
-                axi_arvalid_int = 1'b1;
-              default: // load_process
-                axi_rready_int  = 1'b1;
+               idle:         replace = 1'b0;
+               init_process: axi_arvalid_int = 1'b1;
+               default:      axi_rready_int = 1'b1;  // load_process
             endcase
          end
-         
+
       end else begin
          // Constant AXI signals
-         assign axi_arid_o    = `IOB_CACHE_AXI_ID;
-         assign axi_arlock_o  = 1'b0;
+         assign axi_arid_o = `IOB_CACHE_AXI_ID;
+         assign axi_arlock_o = 1'b0;
          assign axi_arcache_o = 4'b0011;
-         assign axi_arprot_o  = 3'd0;
-         assign axi_arqos_o   = 4'd0;
+         assign axi_arprot_o = 3'd0;
+         assign axi_arqos_o = 4'd0;
 
          // Burst parameters - single
-         assign axi_arlen_o   = 8'd0;        // A single burst of Memory data width word
+         assign axi_arlen_o = 8'd0;  // A single burst of Memory data width word
          assign axi_arsize_o  = `IOB_CACHE_BE_NBYTES_W; // each word will be the width of the memory for maximum bandwidth
          assign axi_arburst_o = 2'b00;
          assign axi_araddr_o  = {BE_ADDR_W{1'b0}} + {replace_addr, {`IOB_CACHE_BE_NBYTES_W{1'b0}}}; // base address for the burst, with width extension
 
          // Read Line values
          assign read_valid = axi_rvalid_i;
-         assign read_rdata  = axi_rdata_i;
+         assign read_rdata = axi_rdata_i;
 
-         localparam
-           idle          = 2'd0,
-           init_process  = 2'd1,
-           load_process  = 2'd2,
-           end_process   = 2'd3;
+         localparam idle = 2'd0, init_process = 2'd1, load_process = 2'd2, end_process = 2'd3;
 
-         reg [1:0]                           state;
+         reg [1:0] state;
 
          always @(posedge clk_i, posedge reset) begin
-            if (reset)
-              state <= idle;
+            if (reset) state <= idle;
             else
-              case (state)
-                idle: begin
-                   if (replace_valid)
-                     state <= init_process;
-                   else
-                     state <= idle;
-                end
-                init_process: begin
-                   if (axi_arready_i)
-                     state <= load_process;
-                   else
-                     state <= init_process;
-                end
-                load_process: begin
-                   if (axi_rvalid_i)
-                     if (axi_rresp_i != 2'b00) // slave_error - received at the same time as valid
-                       state <= init_process;
-                     else
-                       state <= end_process;
-                   else
-                     state <= load_process;
-                end
-                end_process: // delay for the read_latency of the memories (if the rdata is the last word)
+               case (state)
+                  idle: begin
+                     if (replace_valid) state <= init_process;
+                     else state <= idle;
+                  end
+                  init_process: begin
+                     if (axi_arready_i) state <= load_process;
+                     else state <= init_process;
+                  end
+                  load_process: begin
+                     if (axi_rvalid_i)
+                        if (axi_rresp_i != 2'b00) // slave_error - received at the same time as valid
+                           state <= init_process;
+                        else state <= end_process;
+                     else state <= load_process;
+                  end
+                  end_process: // delay for the read_latency of the memories (if the rdata is the last word)
                   state <= idle;
-                default:;
-              endcase
+                  default: ;
+               endcase
          end
 
          always @* begin
-            axi_arvalid_int   = 1'b0;
-            axi_rready_int    = 1'b0;
-            replace = 1'b1;
+            axi_arvalid_int = 1'b0;
+            axi_rready_int  = 1'b0;
+            replace         = 1'b1;
 
             case (state)
-              idle: begin
-                 replace = 1'b0;
-              end
-              init_process: begin
-                 axi_arvalid_int = 1'b1;
-              end
-              load_process: begin
-                 axi_rready_int  = 1'b1;
-              end
-              default:;
+               idle: begin
+                  replace = 1'b0;
+               end
+               init_process: begin
+                  axi_arvalid_int = 1'b1;
+               end
+               load_process: begin
+                  axi_rready_int = 1'b1;
+               end
+               default: ;
             endcase
          end
       end
