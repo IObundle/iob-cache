5f960b1d45d2be7538a9d503cf312c8012e8afc4
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-09-21 16:36:14 +0100

    refactor(swreg): Use iob_cache_swreg.h instead of manual defines.

--- software/src/iob-cache.h
+++ software/src/iob-cache.h
@@ -1,21 +1,10 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+#include "iob_cache_swreg.h"
+
 #define CACHEFUNC(cache_base, func)                                            \
   (*((volatile int *)(cache_base + (func * sizeof(int)))))
-
-// Function's memory map
-#define BUFFER_EMPTY 0
-#define BUFFER_FULL 1
-#define HIT 4
-#define MISS 8
-#define READ_HIT 12
-#define READ_MISS 16
-#define WRITE_HIT 20
-#define WRITE_MISS 24
-#define COUNTER_RESET 28
-#define INVALIDATE 29
-#define CACHE_VERSION 30
 
 // Cache Controllers's functions
 void cache_init(int ext_mem,
