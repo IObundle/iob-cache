7d6173cf3d2ed2f7ff23fae72fb2821281f9852a
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-12-21 04:51:44 +0000

    fix(pc_emul): Fix pc_emul function names

--- software/src/iob_cache_swreg_pc_emul.c
+++ software/src/iob_cache_swreg_pc_emul.c
@@ -6,25 +6,25 @@ static int base;
 void IOB_CACHE_INIT_BASEADDR(uint32_t addr) { base = addr; }
 
 // Core Setters
-void IOB_CACHE_SET_CACHE_RST_CNTRS(uint8_t value) { return; }
+void IOB_CACHE_SET_RST_CNTRS(uint8_t value) { return; }
 
-void IOB_CACHE_SET_CACHE_INVALIDATE(uint8_t value) { return; }
+void IOB_CACHE_SET_INVALIDATE(uint8_t value) { return; }
 
 // Core Getters
-uint8_t IOB_CACHE_GET_CACHE_WTB_EMPTY() { return 1; }
+uint8_t IOB_CACHE_GET_WTB_EMPTY() { return 1; }
 
-uint8_t IOB_CACHE_GET_CACHE_WTB_FULL() { return 0; }
+uint8_t IOB_CACHE_GET_WTB_FULL() { return 0; }
 
-uint32_t IOB_CACHE_GET_CACHE_RW_HIT() { return 0; }
+uint32_t IOB_CACHE_GET_RW_HIT() { return 0; }
 
-uint32_t IOB_CACHE_GET_CACHE_RW_MISS() { return 0; }
+uint32_t IOB_CACHE_GET_RW_MISS() { return 0; }
 
-uint32_t IOB_CACHE_GET_CACHE_READ_HIT() { return 0; }
+uint32_t IOB_CACHE_GET_READ_HIT() { return 0; }
 
-uint32_t IOB_CACHE_GET_CACHE_READ_MISS() { return 0; }
+uint32_t IOB_CACHE_GET_READ_MISS() { return 0; }
 
-uint32_t IOB_CACHE_GET_CACHE_WRITE_HIT() { return 0; }
+uint32_t IOB_CACHE_GET_WRITE_HIT() { return 0; }
 
-uint32_t IOB_CACHE_GET_CACHE_WRITE_MISS() { return 0; }
+uint32_t IOB_CACHE_GET_WRITE_MISS() { return 0; }
 
-uint32_t IOB_CACHE_GET_CACHE_VERSION() { return 0x0010; }
+uint16_t IOB_CACHE_GET_VERSION() { return 0x0010; }
