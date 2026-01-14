ffd538ab6589906753d95112b662ccb9dd6fcd3e
P-Miranda <pmiranda.iobundle@gmail.com>
2026-01-14 17:27:01 +0000

    fix(ug): remove orphaned UG section - update py2hwsw commit - remove orphaned UG section - update UG

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz") {} }:
 
 let
-  py2hwsw_commit = "0ec68b485178b0fe63096482301a75f93bc2014e"; # Replace with the desired commit.
-  py2hwsw_sha256 = "Vc7SRiomN8DyylpzxvFsZky13lkQ0ALGOgSK0dxyY1s="; # Replace with the actual SHA256 hash.
+  py2hwsw_commit = "2b4512745dcd301a26cdd24aed7b87d68d043c66"; # Replace with the desired commit.
+  py2hwsw_sha256 = "GOIjjRb2nuNwuHh1ai6iwmh2idicP1UVoYAY9HUhktA="; # Replace with the actual SHA256 hash.
   # Get local py2hwsw root from `PY2HWSW_ROOT` env variable
   py2hwswRoot = builtins.getEnv "PY2HWSW_ROOT";
 
